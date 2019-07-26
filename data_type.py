c8941b7134f8b02f4b41c768c40873e1897d8d10
Omar <tachomi@hotmail.com>
2019-07-25 20:04:08 -0500

    Adding more data types

--- data_type.py
+++ data_type.py
@@ -1,8 +1,38 @@
 # Strings 
+
 print('Hello World!')
 print("Hello World!")
-print(type("Hello World"))
-print(type(100))
+print(type("Hello World")) # The result will be str
+
 
 # Number
 
+print(type(30)) # The result will be int
+print(type(30.5)) # The result will be float
+
+# Boolean
+
+print(type(True)) # The result will be bool
+print(type(False)) # The result will be bool
+
+# List 
+
+print(type([10, 20, 30]))
+print(type(['Hello', 'Hi', 'Aloha']))
+print(type(['hello', 10, 10.5, True]))
+
+# Tuples
+
+print(type((10, 20, 30)))
+
+# directories
+
+print(type({
+    "Name":"Omar",
+    "Lastname":"Aguilar Rosas",
+    "nickname":"Tachomi"
+}))
+{
+    "lat": 1213541245,
+    "long": 619431421
+}
