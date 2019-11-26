e089c480bda53dec834085fa46556c9753f542db
tachomi <tachomi@hotmail.com>
2019-11-26 15:09:49 -0600

    creating simple story with variables

---
+++ story.py
@@ -0,0 +1,7 @@
+character_age = "35"
+character_name = "John"
+
+print("There once was a man named " + character_name + ", ")
+print("he was " + character_age + " years old. ")
+print("He really liked the name " + character_name + ", ")
+print("but didn't like being " + character_age + ". ")
